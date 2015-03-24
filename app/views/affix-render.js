85d1a592abc9992d08aac22671feb89cd3967195
j.corns <jason.corns@classmates.com>
2015-03-24 13:26:12 -0700

    JiraId: UI-2051 view handlers for affix menu

--- app/views/affix-render.js
+++ app/views/affix-render.js
@@ -3,14 +3,27 @@ import Ember from 'ember';
 export default Ember.View.extend({
   affixRender: function () {
     Ember.run.schedule('afterRender', function () {
-      $('.bs-docs-sidebar').affix({
-        offset: {
-          top: 340,
-          bottom: function () {
-            return (this.bottom = $('.footer').outerHeight(true))
+      $('.bs-docs-sidebar')
+        .affix({
+          offset: {
+            top: 340,
+            bottom: function () {
+              return (this.bottom = $('.footer').outerHeight(true))
+            }
           }
-        }
-      });
+        }).find('a').on('click.docs.scrollTo', function (evt) {
+          var sel = $(this).attr('href'),
+            $target = $(sel);
+          if($target.length>0) {
+            $('html, body').animate({
+              scrollTop: $target.offset().top
+            }, 200);
+          }
+          evt.preventDefault();
+        });
     });
-  }.on('didInsertElement')
+  }.on('didInsertElement'),
+  affixTearDown: function () {
+    $('.bs-docs-sidebar').find('a').off('click.docs.scrollTo');
+  }.on('willDestroyElement')
 });
